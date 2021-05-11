fdaadb17140261611299c9b67e91e704017f71ea
nikita-kochnev <83593694+nikita-kochnev@users.noreply.github.com>
2021-05-11 20:20:54 +0300

    Update balloon.py
    Changed loading of images and masks to fit premade versions

--- samples/balloon/balloon.py
+++ samples/balloon/balloon.py
@@ -69,7 +69,7 @@ class BalloonConfig(Config):
     NUM_CLASSES = 1 + 1  # Background + balloon
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 100
+    STEPS_PER_EPOCH = 10
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
@@ -92,6 +92,14 @@ class BalloonDataset(utils.Dataset):
         # Train or validation dataset?
         assert subset in ["train", "val"]
         dataset_dir = os.path.join(dataset_dir, subset)
+       
+        image_ids = next(os.walk(dataset_dir))[2]
+              
+        for image_id in image_ids:
+            self.add_image(
+                "balloon",
+                image_id=image_id,
+                path=os.path.join(dataset_dir, image_id))
 
         # Load annotations
         # VGG Image Annotator (up to version 1.6) saves each image in the form:
@@ -109,37 +117,38 @@ class BalloonDataset(utils.Dataset):
         # }
         # We mostly care about the x and y coordinates of each region
         # Note: In VIA 2.0, regions was changed from a dict to a list.
-        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
-        annotations = list(annotations.values())  # don't need the dict keys
+        
+        # annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
+        # annotations = list(annotations.values())  # don't need the dict keys
 
         # The VIA tool saves images in the JSON even if they don't have any
         # annotations. Skip unannotated images.
-        annotations = [a for a in annotations if a['regions']]
+        # annotations = [a for a in annotations if a['regions']]
 
         # Add images
-        for a in annotations:
+        # for a in annotations:
             # Get the x, y coordinaets of points of the polygons that make up
             # the outline of each object instance. These are stores in the
             # shape_attributes (see json format above)
             # The if condition is needed to support VIA versions 1.x and 2.x.
-            if type(a['regions']) is dict:
-                polygons = [r['shape_attributes'] for r in a['regions'].values()]
-            else:
-                polygons = [r['shape_attributes'] for r in a['regions']] 
+            # if type(a['regions']) is dict:
+                # polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            # else:
+                # polygons = [r['shape_attributes'] for r in a['regions']] 
 
             # load_mask() needs the image size to convert polygons to masks.
             # Unfortunately, VIA doesn't include it in JSON, so we must read
             # the image. This is only managable since the dataset is tiny.
-            image_path = os.path.join(dataset_dir, a['filename'])
-            image = skimage.io.imread(image_path)
-            height, width = image.shape[:2]
+            # image_path = os.path.join(dataset_dir, a['filename'])
+            # image = skimage.io.imread(image_path)
+            # height, width = image.shape[:2]
 
-            self.add_image(
-                "balloon",
-                image_id=a['filename'],  # use file name as a unique image id
-                path=image_path,
-                width=width, height=height,
-                polygons=polygons)
+            # self.add_image(
+                # "balloon",
+                # image_id=a['filename'],  # use file name as a unique image id
+                # path=image_path,
+                # width=width, height=height,
+                # polygons=polygons)
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
@@ -149,23 +158,37 @@ class BalloonDataset(utils.Dataset):
         class_ids: a 1D array of class IDs of the instance masks.
         """
         # If not a balloon dataset image, delegate to parent class.
-        image_info = self.image_info[image_id]
-        if image_info["source"] != "balloon":
-            return super(self.__class__, self).load_mask(image_id)
+        # image_info = self.image_info[image_id]
+        # if image_info["source"] != "balloon":
+            # return super(self.__class__, self).load_mask(image_id)
 
         # Convert polygons to a bitmap mask of shape
         # [height, width, instance_count]
         info = self.image_info[image_id]
-        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
-                        dtype=np.uint8)
-        for i, p in enumerate(info["polygons"]):
+       
+        mask_dir = os.path.join(os.path.dirname(os.path.dirname(info['path'])), "masks")
+
+        # Read mask files from .png image
+        mask = []
+        for f in next(os.walk(mask_dir))[2]:
+            if f.endswith(".png"):
+                m = skimage.io.imread(os.path.join(mask_dir, f)).astype(np.bool)
+                mask.append(m)
+        mask = np.stack(mask, axis=-1)
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID, we return an array of ones
+        return mask, np.ones([mask.shape[-1]], dtype=np.int32)
+       
+        # mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
+                        # dtype=np.uint8)
+        # for i, p in enumerate(info["polygons"]):
             # Get indexes of pixels inside the polygon and set them to 1
-            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
-            mask[rr, cc, i] = 1
+            # rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+            # mask[rr, cc, i] = 1
 
         # Return mask, and array of class IDs of each instance. Since we have
         # one class ID only, we return an array of 1s
-        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
+        # return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
 
     def image_reference(self, image_id):
         """Return the path of the image."""
