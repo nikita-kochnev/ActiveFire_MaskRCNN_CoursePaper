c30a282fcac3f5ae897e0afc5ad96011d3c90c5d
nikita-kochnev <83593694+nikita-kochnev@users.noreply.github.com>
2021-05-17 19:26:30 +0300

    Update balloon.py

--- samples/balloon/balloon.py
+++ samples/balloon/balloon.py
@@ -172,7 +172,7 @@ class BalloonDataset(utils.Dataset):
         mask = []
         # for f in next(os.walk(mask_dir))[2]:
             # if f.endswith(".png"):
-        m = skimage.io.imread(os.path.join(mask_dir, info['image_id'])).astype(np.bool)
+        m = skimage.io.imread(os.path.join(mask_dir, info['id'])).astype(np.bool)
         mask.append(m)
         mask = np.stack(mask, axis=-1)
         # Return mask, and array of class IDs of each instance. Since we have
